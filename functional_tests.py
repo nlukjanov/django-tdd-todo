0813bee60df8c77b9b9a960fd8caeaaa94f4a03e
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-05-27 20:05:34 +0100

    add cleanup

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
 browser = webdriver.Chrome('/usr/local/bin/chromedriver')
-browser.get('http://localhost:8000')
-assert 'To-Do' in browser.title._f'browser title was {browser.title}'
+browser.get('http://localhost:3000')
+try:
+    assert 'To-Do' in browser.title, "Browser title was " + browser.title
+finally:
+    browser.close()
+    browser.quit()
