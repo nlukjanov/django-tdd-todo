57f76893efb1bf23a3c5e2169d7b267d198ed6f3
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-05-28 12:57:25 +0100

    refactor to use template

--- lists/tests.py
+++ lists/tests.py
@@ -10,4 +10,5 @@ class HomePageTest(TestCase):
             '<title>To-Do lists</title>',
             response.content.decode()
         )
-        self.assertTrue(response.content.decode().endswith('</html>'))
+        self.assertTrue(response.content.decode().strip().endswith('</html>'))
+        self.assertTemplateUsed(response, 'home`.html')
