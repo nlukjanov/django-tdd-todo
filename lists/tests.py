7fcf5c1c22e31a6f65fc74e0c5c98e3f1b417c60
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-06-01 14:58:56 +0100

    add item to the table

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,10 @@ from django.test import TestCase
 
 class HomePageTest(TestCase):
 
-    def test_smoke_test(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home`.html')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_handles_post_request(self):
+        response = self.client.post('/', {'item_text': 'a list item'})
+        self.assertIn('a list item', response.content.decode())
