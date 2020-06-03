6480afa1b525d9755aa2e63bbb193dc0718c345f
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-06-03 10:25:51 +0100

    change item name

--- lists/tests.py
+++ lists/tests.py
@@ -25,13 +25,13 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
 
         response = self.client.get('/')
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
