ac2e849dd3cc791b2d2b79093c13fd47ffe20b9a
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-09-08 12:31:14 +0100

    refactor unit tests to separate models from views

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -38,39 +38,10 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 2')
 
     def test_passed_correct_list_to_template(self):
-        other_list = List.objects.create()
+        # other_list = List.objects.create()
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
-
-
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, list_)
 
 
 class NewListTest(TestCase):
@@ -90,7 +61,7 @@ class NewListTest(TestCase):
 
 class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
+        # other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
@@ -104,7 +75,7 @@ class NewItemTest(TestCase):
         self.assertEqual(new_item.list, correct_list)
 
     def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
+        # other_list = List.objects.create()
         correct_list = List.objects.create()
 
         response = self.client.post(
