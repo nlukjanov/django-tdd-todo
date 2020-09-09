36a8bbb63eafdcecb0197446f39f7b1ed12f7fde
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-09-09 15:03:41 +0100

    implement returning error if list is empty

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,7 @@
 # pylint: disable=no-member
 from django.test import TestCase
 from lists.models import Item, List
+from django.utils.html import escape
 
 
 class HomePageTest(TestCase):
@@ -38,7 +39,6 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 2')
 
     def test_passed_correct_list_to_template(self):
-        # other_list = List.objects.create()
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
@@ -58,10 +58,17 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
 
 class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
-        # other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
@@ -75,7 +82,6 @@ class NewItemTest(TestCase):
         self.assertEqual(new_item.list, correct_list)
 
     def test_redirects_to_list_view(self):
-        # other_list = List.objects.create()
         correct_list = List.objects.create()
 
         response = self.client.post(
