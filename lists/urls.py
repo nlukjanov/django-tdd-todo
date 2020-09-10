e39b06e794f4b67c1947ccccc467a23d8122701a
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-09-10 10:45:21 +0100

    remove hardcoded urls from templates

--- lists/urls.py
+++ lists/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import view_list, new_list
 
 urlpatterns = [
-    path('new', new_list),
-    path('<int:list_id>/', view_list)
+    path('new', new_list, name='new_list'),
+    path('<int:list_id>/', view_list, name='view_list')
 ]
