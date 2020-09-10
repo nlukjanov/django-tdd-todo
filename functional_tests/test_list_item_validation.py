260e090f8928fba6ef91e8443392c4918000b30f
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-09-10 08:42:53 +0100

    fix css selector

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -12,14 +12,14 @@ class ItemValidationTest(FunctionalTest):
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector(
-            '.has_error').text, "You can't have an empty list item"))
+            '.has-error').text, "You can't have an empty list item"))
 
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector(
-            '.has_error').text, "You can't have an empty list item"))
+            '.has-error').text, "You can't have an empty list item"))
 
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
